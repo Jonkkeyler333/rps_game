14034915b3d7ad963e42cfb86ffc7bd7d49b9774
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2023-08-11 15:51:58 -0500

    botones añadidos

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 let botton=document.getElementById("b1")
 let game_home=document.getElementById("inicio")
+let game=document.getElementById("game")
 botton.addEventListener("click",iniciar_juego)
+let partidas=0
 
 function iniciar_juego()
 {
     game_home.style.display="none"
+    partidas=prompt('Ingresa por favor el número de partidas que deseas jugar')
+    game.style.visibility="visible"
 }
