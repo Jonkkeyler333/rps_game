479b86cb3d1a75c7e9bbe6a8128140fd3d9b63fd
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2023-08-11 15:39:10 -0500

    some style changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 let botton=document.getElementById("b1")
+let game_home=document.getElementById("inicio")
 botton.addEventListener("click",iniciar_juego)
 
 function iniciar_juego()
 {
-    
+    game_home.style.display="none"
 }
