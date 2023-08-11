0030a714988183997a8792d44244f1597c9a0d1a
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2023-08-11 16:14:45 -0500

    botones funcionales y verificación del número de partidas

--- js/script.js
+++ js/script.js
@@ -7,6 +7,29 @@ let partidas=0
 function iniciar_juego()
 {
     game_home.style.display="none"
-    partidas=prompt('Ingresa por favor el número de partidas que deseas jugar')
     game.style.visibility="visible"
+    temp=parseInt(prompt('Ingresa por favor el número de partidas que deseas jugar'))
+    if (isNaN(temp))
+    {
+        alert('Has ingresado un dato invalido😒,Intentalo otra vez ')
+        temp=prompt('Ingresa por favor el número de partidas que deseas jugar')
+        partidas=parseInt(temp)
+    }
+    else
+    {
+        partidas=temp
+    }
+    console.log(typeof(partidas)+" "+partidas)
+}
+
+// 1->👊🏽 2->📄 3->✂️
+jugador=0
+document.getElementById("piedra").addEventListener("click",eleccion)
+document.getElementById("papel").addEventListener("click",eleccion)
+document.getElementById("tijera").addEventListener("click",eleccion)
+
+function eleccion(event)
+{
+    let boton_p=event.target.id
+    console.log(boton_p)
 }
