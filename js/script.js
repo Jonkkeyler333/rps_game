840727dce1ece5fcc042d289adb7cb3917d677b6
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2023-08-12 15:13:12 -0500

    imagenes agregadas

--- js/script.js
+++ js/script.js
@@ -22,14 +22,51 @@ function iniciar_juego()
     console.log(typeof(partidas)+" "+partidas)
 }
 
+
+//Juego principal
 // 1->👊🏽 2->📄 3->✂️
-jugador=0
+let jugador=0
+con=0
 document.getElementById("piedra").addEventListener("click",eleccion)
 document.getElementById("papel").addEventListener("click",eleccion)
 document.getElementById("tijera").addEventListener("click",eleccion)
 
+function aleatorio(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + 1)
+}
+let computadora=aleatorio(1,3)
+
 function eleccion(event)
 {
     let boton_p=event.target.id
-    console.log(boton_p)
+    if(boton_p==="piedra")
+    {
+        jugador=1
+    }
+    else if(boton_p==="papel")
+    {
+        jugador=2
+    }
+    else
+    {
+        jugador=3
+    }
+    console.log(boton_p+" "+jugador)
+    gameplay()
+}
+
+let puntaje_j=0  //jugador
+let puntaje_p=0  //pc
+
+function gameplay()
+{
+    if(con==3)
+    {
+        alert('juego acabado')
+    }
+    else
+    {
+        console.log("el jugador saco "+jugador+" y el pc "+computadora)
+        con++
+    }
 }
